6317e7b0a25fb2b9f9758e2e2af84e5d4407dbbb
Gil <gil@wecontent.com.br>
2022-07-20 00:26:10 -0300

    fixing gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,23 +25,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <React.Fragment>
-      <Script id="Adsense-id" 
-        onError={(e) => { console.error("Script failed to load", e);}}
-        data-ad-client="ca-pub-5396947550532993"
-        strategy="beforeInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        crossOrigin="anonymous" />
-      <Script id="GTM-id" strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-M88QS0CYD8" />
-      <Script id="GTM-datalayer-id" strategy="beforeInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-M88QS0CYD8', {
-            page_path: window.location.pathname,
-            });
-        `}
-      </Script>
+      <Script id="gtm-id" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NHBJHZ5');`}}></Script>
+
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHBJHZ5"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
       <Layout>
         <Component {...pageProps} />
       </Layout>
